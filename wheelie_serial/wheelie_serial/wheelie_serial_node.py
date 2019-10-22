ff32b9a4ce0a645f7617c3a84a6b2719a47aafb4
conroy-cheers <cheers.conroy@gmail.com>
2019-10-22 17:51:43 +1100

    Add twist publisher

--- wheelie_serial/wheelie_serial/wheelie_serial_node.py
+++ wheelie_serial/wheelie_serial/wheelie_serial_node.py
@@ -2,6 +2,7 @@ from enum import IntEnum
 import random
 import struct
 import sys
+import time
 
 import serial.tools.list_ports
 import serial.threaded
@@ -12,6 +13,7 @@ from rclpy.node import Node
 from std_srvs.srv import SetBool
 from sensor_msgs.msg import Imu
 from std_msgs.msg import String
+from geometry_msgs.msg import TwistStamped
 
 
 class SerialSendMessageType(IntEnum):
@@ -23,21 +25,25 @@ class SerialSendMessageType(IntEnum):
 
 
 class WheelieSerialPacketizer(serial.threaded.Packetizer):
-    TERMINATOR = b'\0'
+    TERMINATOR = b'\x8f\x46\x3a\x3a'
 
     def __init__(self, packet_handler, logger):
+        super(WheelieSerialPacketizer, self).__init__()
         self.packet_handler = packet_handler
         self.logger = logger
+
+    def __call__(self, *args, **kwargs):
+        return self
 
     def connection_made(self, transport):
         self.logger.info("Serial connected")
 
     def connection_lost(self, exc):
-        self.logger.error("Serial connection lost. Exiting")
+        self.logger.error(str(exc))
+        # self.logger.error("Serial connection lost. Exiting")
         sys.exit(1)
 
     def handle_packet(self, packet):
-        print("Received packet", packet)
         self.packet_handler(packet)
 
 
@@ -63,6 +69,13 @@ class WheelieSerial(Node):
 
         self.srv = self.create_service(SetBool, '~/set_motors_enabled', self.set_motors_enabled_callback)
 
+        self.twist_sub = self.create_subscriber(TwistStamped, '~/twist_target', self.twist_callback, 10)
+        self.last_twist_time = 0
+
+        self.movement_timeout = 0.5  # seconds
+        self.movement_enabled = False
+        self.movement_timer = self.create_timer(self.movement_timeout, self.movement_timer_callback)
+
         ser_ports = [
             p.device
             for p in serial.tools.list_ports.comports()
@@ -71,17 +84,24 @@ class WheelieSerial(Node):
             self.get_logger().error("No serial port found. Exiting")
             sys.exit(1)
         self.get_logger().info(f"Found serial ports: {', '.join(ser_ports)}")
+
+        if "/dev/cu.usbmodem14101" in ser_ports:
+            ser_ports[0] = "/dev/cu.usbmodem14101"
         self.get_logger().info(f"Using {ser_ports[0]}")
 
         ser = serial.Serial(ser_ports[0], baudrate=115200, timeout=1)
         packetizer = WheelieSerialPacketizer(self.incoming_serial_handler, self.get_logger())
-        self.serial_thread = serial.threaded.ReaderThread(ser, lambda: packetizer)
+        self.serial_thread = serial.threaded.ReaderThread(ser, packetizer)
 
         self.serial_thread.start()
         self.transport, self.protocol = self.serial_thread.connect()
 
     def imu_timer_callback(self):
         self.imu_publisher.publish(self.imu_msg)
+
+    def movement_timer_callback(self):
+        if time.time() - self.last_twist_time > 0.5:
+            self.movement_enabled = False
 
     @staticmethod
     def bytes_checksum(b):
@@ -90,19 +110,33 @@ class WheelieSerial(Node):
             sum += int(byte)
         return sum
 
-    def incoming_serial_handler(self, packet):
-        msg_id, msg_type, int1, int2, int3, check = self.incoming_msg.unpack(packet)
+    @staticmethod
+    def int_to_float(i):
+        return i / 1000
 
-        if check != self.bytes_checksum(self.incoming_msg.pack(msg_id, msg_type, int1, int2, int3, 0)):
-            self.get_logger().error("Incorrect checksum! Ignoring message")
+    @staticmethod
+    def float_to_int(f):
+        return int(f * 1000)
+
+    def incoming_serial_handler(self, packet):
+        if len(packet) != 24:
+            self.get_logger().error(f"Received message with incorrect length {len(packet)}. Ignoring.")
+            return
+        msg_id, msg_type, int1, int2, int3, check = self.incoming_msg.unpack(packet)
+        # self.get_logger().info(f"Received message: {msg_id} {msg_type} {int1} {int2} {int3} {check}")
+
+        calced_checksum = self.bytes_checksum(self.incoming_msg.pack(msg_id, msg_type, int1, int2, int3, 0))
+        if check != calced_checksum:
+            self.get_logger().error(f"Incorrect checksum {calced_checksum} != {check}. Ignoring message")
             return
 
         if msg_type == 0:
             # Acceleration values
-            self.imu_msg.x, self.imu_msg.y, self.imu_msg.z = int1, int2, int3
+            self.imu_msg.linear_acceleration.x, self.imu_msg.linear_acceleration.y, \
+            self.imu_msg.linear_acceleration.z = \
+                self.int_to_float(int1), self.int_to_float(int2), self.int_to_float(int3)
 
     def send_serial_bytes(self, b):
-        print(list(bytearray(b)))
         self.transport.write(b + b'\0')
 
     @staticmethod
@@ -112,18 +146,30 @@ class WheelieSerial(Node):
     def format_serial_message(self, msg_id, msg_type, int1=0, int2=0):
         chk = self.bytes_checksum(self.msg.pack(msg_id, msg_type, int1, int2, 0))
         full_msg = self.msg.pack(msg_id, msg_type, int1, int2, chk)
-        print(self.msg.unpack(full_msg))
         return full_msg
 
     def set_motors_enabled_callback(self, request, response):
+        enabled = bool(request.data)
+        self.movement_enabled = enabled
+
         self.send_serial_bytes(
-            self.format_serial_message(self.generate_id(), SerialSendMessageType.SET_POWER, int(request.data)))
+            self.format_serial_message(self.generate_id(), SerialSendMessageType.SET_POWER, int(enabled)))
 
         # TODO wait for ACK before sending response
         response.success = True
         response.message = ""
 
         return response
+
+    def twist_callback(self, msg):
+        if self.movement_enabled:
+            self.last_twist_time = time.time()
+            self.send_serial_bytes(
+                self.format_serial_message(self.generate_id(), SerialSendMessageType.SET_VELOCITY,
+                                           msg.twist.linear.x, msg.twist.angular.z)
+            )
+        else:
+            self.get_logger().warn('Twist target received, but movement not enabled currently.')
 
     def listener_callback(self, msg):
         self.get_logger().info('I heard: "%s"' % msg.data)
